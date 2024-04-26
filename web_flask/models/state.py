344350f11bd15d1163d259c1561b8ca2592641d6
Abele-n <otienoabele89@gmail.com>
2024-04-26 04:00:44 -0700

    web flask

---
+++ web_flask/models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""State class definition"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """The State class representation
+
+    Attributes:
+    name (str): state name
+    """
+    name = ""
