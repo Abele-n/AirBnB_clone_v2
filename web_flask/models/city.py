1803c998876f0bad549233c3290dca617b8eafb9
Abele-n <otienoabele89@gmail.com>
2024-04-29 02:23:50 -0700

    updated

--- web_flask/models/city.py
+++ web_flask/models/city.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-"""City class definition"""
+"""City class definition."""
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """
-    City class representation
+    Define the city class representation.
 
     Attributes:
     state_id (str): state id
     name(str): city name
     """
-    state_id = ""
-    name = ""
+
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
